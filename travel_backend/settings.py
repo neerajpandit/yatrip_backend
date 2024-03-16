ebebda4a4f6334081585a26499e4ea119103b510
neerajpandit <pandeyneeraj1507@gmail.com>
2024-03-16 15:10:07 +0530

    fix eror

--- travel_backend/settings.py
+++ travel_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECRET_KEY="django-insecure-c742)h-iq%u^^whrn@pk^oi6=(nujy04u$@vm03$aj)t+kyeq)"
 
 # 'django-insecure-c742)h-iq%u^^whrn@pk^oi6=(nujy04u$@vm03$aj)t+kyeq)'
